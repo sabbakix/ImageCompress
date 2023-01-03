7667fbb60b8789d09af9b5fb098913232a3ce5a0
Stefy Sabbadin <9255011+sabbakix@users.noreply.github.com>
2023-01-03 23:50:49 +0100

    Restore from minimized

--- main.js
+++ main.js
@@ -1,40 +1,50 @@
-const { app, BrowserWindow, Menu } = require("electron")
-
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron')
 
 //Set env
 process.env.NODE_ENV = 'development'
 const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
 
-let mainWindow
+let win
 
-function createMainWindow(){
-    const mainWindow = new BrowserWindow({
+app.whenReady().then(() => {
+    win = new BrowserWindow({ 
         title: "ImageCompress",
-        width: 800,
+        width: 800, 
         height: 600,
-        icon: `${__dirname}/assets/icons/compress-icon_225x225.png`,
-        resizable: isDev
+        icon: `${__dirname}/assets/icons/compress-icon_225x225.png`
     })
-    mainWindow.loadFile('./app/index.html')
-}
-
-app.on('ready', () => {
-    createMainWindow()
+    
+    win.loadFile('./app/index.html')
 
     const mainMenu = Menu.buildFromTemplate(menu)
-    Menu.setApplicationMenu(mainMenu)
-    
-    //mainWindow.on('closed',() => mainWindow = null)    
+    //Menu.setApplicationMenu(mainMenu)
+
+    globalShortcut.register('CmdOrCtrl+K', () => {
+        win.restore()
+    })
+
 })
 
+
+
+
+
+
 const menu = [
+    ...(isMac ? [{ role: 'appMenu'}] : []),
     {
         label: 'File',
         submenu: [
             {
                 label: 'Quit',
+                accelerator: 'CmdOrCtrl+W',
                 click: () => app.quit()
+            },
+            {
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+K',
+                click: () => win .reload()
             }
         ]
     }
